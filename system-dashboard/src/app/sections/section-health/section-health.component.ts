62c45023a60ab89c69d5bde8336053c8607d8be4
Oleh <33325458+MatviyO@users.noreply.github.com>
2020-11-07 22:04:41 +0200

    09

--- system-dashboard/src/app/sections/section-health/section-health.component.ts
+++ system-dashboard/src/app/sections/section-health/section-health.component.ts
@@ -1,11 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import {Server} from '../../shared/model/server.model';
 
+
+const ServerData = [
+  {id: 1, name: 'dev-web', isOnline: true},
+  {id: 2, name: 'dev-mail', isOnline: false},
+  {id: 3, name: 'prod-web', isOnline: true}
+];
 @Component({
   selector: 'app-section-health',
   templateUrl: './section-health.component.html',
   styleUrls: ['./section-health.component.css']
 })
 export class SectionHealthComponent implements OnInit {
+
+  servers: Server[] = ServerData;
 
   constructor() { }
 

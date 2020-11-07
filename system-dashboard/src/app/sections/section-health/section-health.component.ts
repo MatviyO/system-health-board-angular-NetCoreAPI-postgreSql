e81bbb1beaf581519512e6caa54c9036d5dcebff
Oleh <33325458+MatviyO@users.noreply.github.com>
2020-11-07 23:06:25 +0200

    10

--- system-dashboard/src/app/sections/section-health/section-health.component.ts
+++ system-dashboard/src/app/sections/section-health/section-health.component.ts
@@ -5,7 +5,8 @@ import {Server} from '../../shared/model/server.model';
 const ServerData = [
   {id: 1, name: 'dev-web', isOnline: true},
   {id: 2, name: 'dev-mail', isOnline: false},
-  {id: 3, name: 'prod-web', isOnline: true}
+  {id: 3, name: 'prod-web', isOnline: true},
+  {id: 4, name: 'prod-mail', isOnline: true}
 ];
 @Component({
   selector: 'app-section-health',
--- system-dashboard/src/app/server/server.component.ts
+++ system-dashboard/src/app/server/server.component.ts
@@ -8,10 +8,27 @@ import {Server} from '../shared/model/server.model';
 })
 export class ServerComponent implements OnInit {
   @Input() server: Server;
+  color: string;
+  buttonText: string;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.getServerAction(this.server.isOnline);
+  }
+  getServerAction(isOnline: boolean): any {
+    if (isOnline) {
+      this.server.isOnline = true;
+      this.color = '#66BB6A';
+      this.buttonText = 'Shut Down';
+    } else {
+      this.server.isOnline = false;
+      this.color = '#FFBB6B';
+      this.buttonText = 'Start';
+    }
+  }
+  toggleStatus(status: boolean): any {
+    this.getServerAction(!status);
   }
 
 }

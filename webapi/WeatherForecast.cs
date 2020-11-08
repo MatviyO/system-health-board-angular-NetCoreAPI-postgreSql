3b6e8a229ea63cf81c1c6b5cdf939e5e71eabf45
Oleh <33325458+MatviyO@users.noreply.github.com>
2020-11-08 12:35:13 +0200

    up

---
+++ webapi/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace system_health_board_angular_NetCoreAPI_postgreSql
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}

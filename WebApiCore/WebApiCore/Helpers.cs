83bd2f3b97771cac2fbc5ecc521ab7355f52f204
Oleh <33325458+MatviyO@users.noreply.github.com>
2020-11-14 23:35:44 +0200

    DFGGDF

--- WebApiCore/WebApiCore/Helpers.cs
+++ WebApiCore/WebApiCore/Helpers.cs
@@ -10,17 +10,64 @@ namespace WebApiCore
 		private static Random _rand = new Random();
 		private static string GetRandom(IList<string> items)
 		{
-			return items[_rand.Next(itesm.Count)];
+			return items[_rand.Next(items.Count)];
 		}
 		internal static string MakeCustomerName()
 		{
 			var prefix = GetRandom(bizPrefix);
-			var sufix = GetRandom(bizSufix);
-			return prefix + sufix;
+			var suffix = GetRandom(bizSuffix);
+			return prefix + suffix;
 		}
-		private static readonly List<string> bizPrefix = new List<string>()
+
+		internal static string MakeCustomerEmail(string customerName)
+		{
+			return $"concact{customerName.ToLower()}.com";
+		}
+
+		internal static string GetRandomState()
 		{
 
 		}
+
+		private static readonly List<string> usStates = new List<string>()
+		{
+			"AK", "AL", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA",
+			"HI", "ID", "IL","IN" ,"IA", "KS", "KY", "LA", "ME", "MD",
+			"MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV","NH", "NJ",
+			"NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC",
+			"SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"
+		};
+	
+
+		private static readonly List<string> bizPrefix = new List<string>()
+		{
+			"ABC",
+			"XYZ",
+			"MainSt",
+			"Sales",
+			"Enterprise",
+			"Ready",
+			"Quick",
+			"Budget",
+			"Peak",
+			"Magic",
+			"Family",
+			"Comfort"
+		};
+		private static readonly List<string> bizSuffix = new List<string>()
+		{
+			"Corporation",
+			"Co",
+			"Logistics",
+			"Transit",
+			"Bakery",
+			"Goods",
+			"Foods",
+			"Cleaners",
+			"Hotels",
+			"Planners",
+			"Automotive",
+			"Books"
+		};
 	}
 }

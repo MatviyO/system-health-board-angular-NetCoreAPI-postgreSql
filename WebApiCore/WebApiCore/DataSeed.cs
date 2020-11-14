b0669125e1f9246bd24399f45ef8c7ad83df3ef9
Oleh <33325458+MatviyO@users.noreply.github.com>
2020-11-14 23:22:48 +0200

    fdgdfg

--- WebApiCore/WebApiCore/DataSeed.cs
+++ WebApiCore/WebApiCore/DataSeed.cs
@@ -51,6 +51,8 @@ namespace WebApiCore
 					Email = email, 
 					State = state
 
+
+
 				});
 			}
 		}
--- WebApiCore/WebApiCore/Helpers.cs
+++ WebApiCore/WebApiCore/Helpers.cs
@@ -7,9 +7,20 @@ namespace WebApiCore
 {
 	public class Helpers
 	{
+		private static Random _rand = new Random();
+		private static string GetRandom(IList<string> items)
+		{
+			return items[_rand.Next(itesm.Count)];
+		}
 		internal static string MakeCustomerName()
 		{
-			return "";
+			var prefix = GetRandom(bizPrefix);
+			var sufix = GetRandom(bizSufix);
+			return prefix + sufix;
+		}
+		private static readonly List<string> bizPrefix = new List<string>()
+		{
+
 		}
 	}
 }

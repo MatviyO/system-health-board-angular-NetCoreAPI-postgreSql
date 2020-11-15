a6fdd43702769a88bddfd703df478fbfb11da792
Oleh <33325458+MatviyO@users.noreply.github.com>
2020-11-15 20:07:01 +0200

    sdds

---
+++ WebApiCore/WebApiCore/Contollers/CustomerController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApiCore.Models;
+
+namespace WebApiCore.Contollers
+{
+	[Route("api/[controller]")]
+	public class CustomerController : Controller
+	{
+		private readonly ApiContext _ctx;
+		public CustomerController(ApiContext ctx)
+		{
+			_ctx = ctx
+		}
+
+		public IActionResult Get()
+		{
+
+		}
+
+	}
+}

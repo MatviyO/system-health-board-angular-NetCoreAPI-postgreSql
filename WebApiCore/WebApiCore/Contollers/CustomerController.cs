5bff6ce172711555167519c4c92b24882a733d4f
Oleh <33325458+MatviyO@users.noreply.github.com>
2020-11-17 23:24:36 +0200

    fdfd

--- WebApiCore/WebApiCore/Contollers/CustomerController.cs
+++ WebApiCore/WebApiCore/Contollers/CustomerController.cs
@@ -22,7 +22,24 @@ namespace WebApiCore.Contollers
 			var data = _ctx.Customers.OrderBy(c => c.Id);
 			return Ok(data);
 		}
-		[HttpGet]
+		[HttpGet("{id}", Name = "GetCustomer")]
+		public IActionResult Get(int id)
+		{
+			var customer = _ctx.Customers.Find(id);
+			return Ok(customer);
+		}
+		[HttpPost]
+		public IActionResult Post([FromBody] Customer customer)
+		{
+			if (customer == null)
+			{
+				return BadRequest();
+			}
+			_ctx.Customers.Add(customer);
+			_ctx.SaveChanges();
+
+			return CreatedAtRoute("GetCustomer", new { id = customer.Id }, customer);
+		}
 
 	}
 }

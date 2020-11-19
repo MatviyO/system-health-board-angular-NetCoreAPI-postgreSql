a37c8e1956bb206bf1a9d378eff3ce0701ad664d
Oleh <33325458+MatviyO@users.noreply.github.com>
2020-11-19 23:35:30 +0200

    gfhf

--- WebApiCore/WebApiCore/Contollers/OrderController.cs
+++ WebApiCore/WebApiCore/Contollers/OrderController.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebApiCore.Models;
 
 namespace WebApiCore.Contollers
 {
+	[Route("api/[controller]")]  
 	public class OrderController : Controller
 	{
-		public IActionResult Index()
+		private readonly ApiContext _ctx;
+		
+		public OrderController(ApiContext ctx)
 		{
-			return View();
+			_ctx = ctx;
 		}
 	}
 }

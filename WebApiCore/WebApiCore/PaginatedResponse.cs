ee2c1ac1f3c3d78af68b4b3e2f725882b0345025
Oleh <33325458+MatviyO@users.noreply.github.com>
2020-11-21 23:29:47 +0200

    15

--- WebApiCore/WebApiCore/PaginatedResponse.cs
+++ WebApiCore/WebApiCore/PaginatedResponse.cs
@@ -5,7 +5,15 @@ using System.Threading.Tasks;
 
 namespace WebApiCore
 {
-	public class PaginatedResponse
+	public class PaginatedResponse<T>
 	{
+		public PaginatedResponse(IEnumerable<T> data, int i, int len)
+		{
+			Data = data.Skip((i - 1) * len).Take(len).ToList();
+			Total = data.Count();
+		}
+
+		public int Total { get; set; }
+		public IEnumerable<T> Data { get; set; }
 	}
 }

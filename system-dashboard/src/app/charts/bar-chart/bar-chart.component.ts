7fdac59341e95a4e1e1c6751a0aa8349f89d7566
Oleh <33325458+MatviyO@users.noreply.github.com>
2020-11-28 20:02:56 +0200

    gfg

--- system-dashboard/src/app/charts/bar-chart/bar-chart.component.ts
+++ system-dashboard/src/app/charts/bar-chart/bar-chart.component.ts
@@ -1,12 +1,13 @@
 import {Component, OnInit} from '@angular/core';
-
-const BarChartData: any[] = [
-  {data: [65, 59, 80, 81, 56, 54, 30], label: 'Q3 Sales'},
-  {data: [25, 39, 60, 91, 36, 54, 50], label: 'Q4 Sales'}
-];
-const BarChartLabels: string[] = [
-  'W1', 'W2', 'W3', 'W4', 'W5', 'W6', 'W7'
-];
+import {SalesDataService} from '../../services/sales-data.service';
+import * as moment from 'moment';
+// const BarChartData: any[] = [
+//   {data: [65, 59, 80, 81, 56, 54, 30], label: 'Q3 Sales'},
+//   {data: [25, 39, 60, 91, 36, 54, 50], label: 'Q4 Sales'}
+// ];
+// const BarChartLabels: string[] = [
+//   'W1', 'W2', 'W3', 'W4', 'W5', 'W6', 'W7'
+// ];
 
 @Component({
   selector: 'app-bar-chart',
@@ -23,12 +24,43 @@ export class BarChartComponent implements OnInit {
   barChartLegend = false;
   barChartType = 'bar';
 
-  constructor() {
+  constructor(private salesService: SalesDataService) {
   }
+  orders; an;
+  orderLabels: string[];
+  orderData: number[];
 
   ngOnInit(): void {
-    this.barChartData = BarChartData;
-    this.barChartLabels = BarChartLabels;
+    this.salesService.getOrders(1, 100)
+      .subscribe(res => {
+        const localChartData = this.getChartData(res);
+        this.barChartLabels = localChartData.map(x => x[0]).reserve();
+        this.barChartData = [{'data': localChartData.map(x => x[1]), 'label': 'Sales'}];
+      });
+  }
+
+  getChartData(res: Response): any {
+    this.orders = res['page']['data'];
+    const data = this.orders.map(o => o.total);
+
+    const formattedOrderss = this.orders.reduce((r, e) => {
+      r.push([moment(e.placed).format('YY-MM-DD'), e.total]);
+      return r;
+    }, []);
+
+    const p = [];
+    const chartData = formattedOrderss.reduce((r, e) => {
+      const key = e[0];
+      if ( !p[key]) {
+        p[key] = e;
+        r.push(p[key]);
+      } else {
+        p[key][1] += e[1];
+      }
+      return r;
+    }, []);
+
+    return chartData;
   }
 
 }
